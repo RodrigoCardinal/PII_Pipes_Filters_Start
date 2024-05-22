439f566ae420ac6b7ec8e0648b84edf8ff2ccdcc
RodrigoCardinal <rodrigo.cardinal@correo.ucu.edu.uy>
2024-05-22 20:53:00 -0300

    Ejercicio 1 Hecho

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,19 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            // Ejercicio 1 Hecho
+            IPipe pipeNull = new PipeNull();
+            IFilter filterNegative = new FilterNegative();
+            IPipe pipeSerial = new PipeSerial(filterNegative, pipeNull);
+            IFilter filterGreyscale = new FilterGreyscale();
+            IPipe pipeSerial2 = new PipeSerial(filterGreyscale, pipeSerial);
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+            picture = pipeSerial2.Send(picture);       
+            provider.SavePicture(picture, @"beer2.jpg");
+            // Ejercicio 2 
+
+            
         }
     }
 }
